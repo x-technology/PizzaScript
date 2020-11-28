4718b2345eb11b5a9f88d13a658ab8af009c4748
Alex Korzhikov <korzio@gmail.com>
2020-11-28 15:22:37 +0100

    add repl

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
-package pizzascript
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pizzascript/pizzascript/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the PizzaScript programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
