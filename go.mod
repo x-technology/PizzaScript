e728ea68e3769048763862f963a8f51c405f1bd8
Alex Korzhikov <korzio@gmail.com>
2021-01-10 09:50:43 +0100

    add draft observable lexer
    add debug configuration
    add initial assets - images, first webinar description

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/cenkalti/backoff/v4 v4.1.0 // indirect
-	github.com/reactivex/rxgo/v2 v2.4.0 // indirect
+	github.com/reactivex/rxgo/v2 v2.4.0
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.6.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,18 +1,88 @@
 package lexer
 
-import "pizzascript/token"
+import (
+	"context"
+	"fmt"
+	"strings"
+	// "sort"
+	"pizzascript/token"
+	"github.com/reactivex/rxgo/v2"
+)
 
 type Lexer struct {
 	input        string
+	observable   rxgo.Observable
 	position     int  // current position in input (points to current char)
 	readPosition int  // current reading position in input (after current char)
 	ch           byte // current char under examination
 }
 
 func New(input string) *Lexer {
-	l := &Lexer{input: input}
-	l.readChar()
+	observable := rxgo.Just(input)().FlatMap(func(i rxgo.Item) rxgo.Observable {
+		splitted := strings.Split(i.V.(string), "")
+		return rxgo.Just(splitted)()
+	})
+
+	l := &Lexer{input: input, observable: observable}
 	return l
+}
+
+func (l *Lexer) Print() {
+	for item := range l.Tokens().Observe() {
+    fmt.Println(item.V)
+	}
+}
+
+func (l *Lexer) Tokens() rxgo.Observable {
+	return rxgo.Merge([]rxgo.Observable{
+		// operators
+		// l.observable.
+		// Filter(func(i interface{}) bool {
+		// 	var str = i.(string)
+
+		// 	sort.Strings(token.ALL_OPERATORS)
+		// 	var index = sort.SearchStrings(token.ALL_OPERATORS, str)
+
+		// 	return token.ALL_OPERATORS[index] == str
+		// }).
+		// Map(func(_ context.Context, i interface{})(interface{}, error) {
+		// 	var str = i.(string)
+		// 	ch := []byte(str)[0]
+		// 	var index = sort.SearchStrings(token.ALL_OPERATORS, str)
+
+		// 	var tok token.Token = newToken(token.TokenType(token.ALL_OPERATORS[index]), ch)
+		// 	return tok, nil
+		// }),
+		// numbers
+		l.observable.
+		// Filter(func(i interface{}) bool {
+		// 	var str = i.(string)
+		// 	ch := []byte(str)[0]
+
+		// 	return isNumber(ch)
+		// }).
+		DistinctUntilChanged(func(_ context.Context, i interface{}) (interface{}, error) {
+			var str = i.(string)
+			ch := []byte(str)[0]
+			fmt.Println(str)
+	
+			return isNumber(ch), nil
+		}).
+		// Reduce(func(_ context.Context, acc interface{}, elem interface{}) (interface{}, error) {
+		// 	if acc == nil {
+		// 		return elem, nil
+		// 	}
+		// 	return acc.(string) + elem.(string), nil
+		// }),
+		// SumInt64()
+		Map(func(_ context.Context, i interface{})(interface{}, error) {
+			var str = i.(string)
+			ch := []byte(str)[0]
+
+			var tok token.Token = newToken(token.INT, ch)
+			return tok, nil
+		}),
+	})
 }
 
 func (l *Lexer) NextToken() token.Token {
