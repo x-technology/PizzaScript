3209c65602ef92394a9c07620239c656b2cee58b
Alex Korzhikov <korzio@gmail.com>
2021-02-12 18:24:40 +0100

    clean code, add lexer tests

--- repl/repl.go
+++ repl/repl.go
@@ -7,7 +7,6 @@ import (
 	"pizzascript/eval"
 	"pizzascript/lexer"
 	"pizzascript/parser"
-	"pizzascript/utils/log"
 )
 
 const PROMPT = "\n>> "
@@ -15,25 +14,6 @@ const PROMPT = "\n>> "
 // Start executes repl, to try PizzaScript with standard input/output
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
-	// debug
-	// input := "1 + 2 * 3"
-	// input := "1 * 2 + 3"
-	// input := "1+2+3+4"
-	// input := "1*2+3*4"
-	// input := "+1"
-	input := "2+1"
-
-	log.Info("lexer & parser example", input)
-
-	l := lexer.New(input)
-	// l.Print()
-
-	p := parser.New(l)
-	p.Print()
-
-	e := eval.New(p)
-	fmt.Println(" = ", e.Eval())
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -44,12 +24,10 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-		// l.Print()
 
 		p := parser.New(l)
-		p.Print()
-		e = eval.New(p)
+		e := eval.New(p)
 
-		fmt.Println(" = ", e.Eval())
+		fmt.Println(e.Eval())
 	}
 }
